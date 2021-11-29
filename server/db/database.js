a37bfd05d6b82b76b017d75cc37bf0e0a552afef
Chase Holt <chaseholt@orc3.com>
2021-11-29 15:58:06 -0500

    changed sequelize server address

--- server/db/database.js
+++ server/db/database.js
@@ -11,8 +11,11 @@ const pkg = require("../package.json");
 console.log(chalk.yellow("Opening database connection"));
 
 // create the database instance that can be used in other database files
-const db = new Sequelize(`postgres://localhost:5432/mycity`, {
-  logging: false, // so we don't see all the SQL queries getting made
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/mycity",
+  {
+    logging: false, // so we don't see all the SQL queries getting made
+  }
+);
 
 module.exports = db;
