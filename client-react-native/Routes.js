48e7bed4a359ab535997616a59149387263e739b
Bradley N. Plunkett <bradleyn.plunkett@Bradleys-MacBook-Pro.local>
2021-11-30 17:38:25 -0500

    added the carousel LFG!

--- client-react-native/Routes.js
+++ client-react-native/Routes.js
@@ -4,6 +4,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from './components/screens/HomeScreen';
 //  import { stack } from '../server/api';
 import SignInScreen from './components/screens/SignInScreen';
+import CarouselCards from './components/screens/CarouselCards';
+import { StyleSheet, SafeAreaView } from "react-native";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +16,7 @@ const Routes = (props) => {
             <Stack.Navigator>
                 <Stack.Screen name='signIn' component={SignInScreen}/>
                 <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="My Groups" component={CarouselCards} />
             </Stack.Navigator>
         </NavigationContainer >
     )
