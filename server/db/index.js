2a287c74189622587415a7808612ad1198b597a5
hector nevarez <hanevarezg@gmail.com>
2021-11-30 14:40:36 -0800

    Cleaned the file

--- server/db/index.js
+++ server/db/index.js
@@ -5,30 +5,16 @@ const Group = require("./models/Group");
 const Tag = require("./models/Tag");
 const Comment= require("./models/Comment");
 
-// Comment.belongsTo(Tag);
-// Tag.hasMany(Comment);
-
-// Comment.belongsTo(Tag);
-
-// User.belongsToMany(Comment, { through: "userComments" });
-// Comment.belongsToMany(User, { through: "userComments" });
 
 User.hasMany(Comment);
 Group.hasMany(Comment);
-// Group.hasMany(Comment);
-// Comment.belongsTo(Group);
+
 Comment.belongsTo(User);
 Comment.belongsTo(Group);
-
 
 Tag.hasMany(Comment);
 Comment.belongsTo(Tag);
 
-// Tag.belongsToMany(Group,{through: "TagList"});
-// Group.belongsToMany(Tag,{through: "TagList"});
-
-// Comment.belongsTo(Group) Error cyclic: Users is dependent of itself 
-// console.log('This is Group magic methods:',Object.keys(Tag.prototype));
 
 module.exports = {
   db,
--- server/db/models/Comment.js
+++ server/db/models/Comment.js
@@ -2,12 +2,6 @@ const Sequelize = require("sequelize");
 const db = require("../database");
 
 const Comment = db.define("comment", {
-    // id: {
-    //     type: Sequelize.INTEGER,
-    //     primaryKey: true,
-    //     autoIncrement: true,
-    //     allowNull: false
-    // },
     description: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -16,22 +10,6 @@ const Comment = db.define("comment", {
             notEmpty: true,
         },
     },
-    // userId: {
-    //     type: Sequelize.INTEGER,
-    //     unique: false,
-    //     allowNull: false,
-    //     validate: {
-    //         notEmpty: true,
-    //     },
-    // },
-    // groupId: {
-    //     type: Sequelize.INTEGER,
-    //     unique: false,
-    //     allowNull: false,
-    //     validate: {
-    //         notEmpty: true,
-    //     },
-    // },
     rating: {
         type: Sequelize.INTEGER,
         allowNull: false,
