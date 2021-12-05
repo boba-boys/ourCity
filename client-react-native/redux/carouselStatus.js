eb624c7beeb827098c0c330a704b19ceac3950ab
hector nevarez <hanevarezg@gmail.com>
2021-12-04 19:04:49 -0800

    Fully merged...for some reason the redux store didnt merged

--- client-react-native/redux/carouselStatus.js
+++ client-react-native/redux/carouselStatus.js
@@ -1,6 +1,5 @@
 
 // action types
-<<<<<<< HEAD
 const GOT_CAROUSEL_STATUS = "GOT_CAROUSEL_STATUS";
 
 // action creators
@@ -8,30 +7,14 @@ const _gotCarouselStatus = (status) => {
   return {
     type: GOT_CAROUSEL_STATUS,
     carouselState: status,
-=======
-const GOT_CarouselStatus = "GOT_CarouselStatus";
-
-// action creators
-const _getCarouselStatus = (status) => {
-  return {
-    type: GOT_CarouselStatus,
-    carouselStatus: status,
->>>>>>> master
   };
 };
 
 // thunks
-<<<<<<< HEAD
 export const getStatus = (currentStatus) => async (dispatch) => {
   try {
     let newStatus=!currentStatus;
     dispatch(_gotCarouselStatus(newStatus));
-=======
-export const getCarouselStatus = (carouselStatus) => async (dispatch) => {
-  try {
-   const newStatus = !carouselStatus
-    dispatch(_getCarouselStatus(newStatus));
->>>>>>> master
   } catch (err) {
     console.log(err);
     return [];
@@ -41,13 +24,8 @@ export const getCarouselStatus = (carouselStatus) => async (dispatch) => {
 // Reducer
 export default function carouselStatus(state = false, action) {
   switch (action.type) {
-<<<<<<< HEAD
     case GOT_CAROUSEL_STATUS:
       return action.carouselState;
-=======
-    case GOT_CarouselStatus:
-      return action.carouselStatus;
->>>>>>> master
     default:
       return state;
   }
