1429715e83a547480c3da9b625d4ef965e43122e
Chase Holt <chaseholt@orc3.com>
2021-12-07 12:43:13 -0500

    GOT SO MANY BUGS KNOCKED DOWN

--- client-react-native/components/screens/CreateTag.js
+++ client-react-native/components/screens/CreateTag.js
@@ -18,7 +18,6 @@ import { addTagStatusFunc } from "../../redux/addTagStatus";
 import { getStatus } from "../../redux/carouselStatus";
 import { getTags } from "../../redux/tags";
 
-
 const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
 const SLIDER_HEIGHT = Dimensions.get("window").height;
@@ -26,50 +25,50 @@ const ITEM_HEIGHT = Math.round(SLIDER_HEIGHT * 0.35);
 
 const CreateTag = () => {
   const userId = useSelector((state) => state.users.id);
-  const coordinates = useSelector((state) => state.addTagCoordinates)
-  const [name, setName] = useState('')
-  const [searchResult, setSearchResult] = useState('')
+  const coordinates = useSelector((state) => state.addTagCoordinates);
+  const [name, setName] = useState("");
+  const [searchResult, setSearchResult] = useState("");
   const dispatch = useDispatch();
-  const groupId = useSelector((state) => state.setGroupIdOnState)
+  const groupId = useSelector((state) => state.setGroupIdOnState);
 
   const navigation = useNavigation();
 
   const onSubmit = async () => {
-
     // console.log('this is theeee name daasdasdasdasda' , name, groupId, userId)
-    dispatch(addTagStatusFunc(true))
-    await axios.post(
-          "https://my-city-server.herokuapp.com/api/tags/addTag",
-          { name, long: coordinates.long, lat:coordinates.lat, groupId , userId}
-        );
-        // dispatch(getStatus(false))
-        dispatch(getTags(groupId))
+    dispatch(addTagStatusFunc(true));
+    await axios.post("https://my-city-server.herokuapp.com/api/tags/addTag", {
+      name,
+      long: coordinates.long,
+      lat: coordinates.lat,
+      groupId,
+      userId,
+    });
+    // dispatch(getStatus(false))
+    dispatch(getTags(groupId));
   };
-
-
 
   return (
     <ScrollView style={styles.container}>
-    <View style={styles.form}>
-      <Text style={styles.title}>Create a Pin</Text>
-      <TextInput
-        style={styles.input}
-        placeholder='Pin Name'
-        value={name}
-        onChangeText={(name) => setName(name)}
-      />
+      <View style={styles.form}>
+        <Text style={styles.title}>Create a Pin</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Pin Name'
+          value={name}
+          onChangeText={(name) => setName(name)}
+        />
 
-      <TextInput
-        style={styles.input}
-        placeholder='Google Search Bar'
-        value={searchResult}
-        onChangeText={setSearchResult}
-      />
-      <TouchableOpacity style={styles.button} onPress={onSubmit}>
-        <Text style={styles.buttonText}>Create Pin</Text>
-      </TouchableOpacity>
-    </View>
-  </ScrollView>
+        <TextInput
+          style={styles.input}
+          placeholder='Google Search Bar'
+          value={searchResult}
+          onChangeText={setSearchResult}
+        />
+        <TouchableOpacity style={styles.button} onPress={onSubmit}>
+          <Text style={styles.buttonText}>Create Pin</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
--- client-react-native/components/screens/SignInScreen.js
+++ client-react-native/components/screens/SignInScreen.js
@@ -1,114 +1,122 @@
-import React, {useState} from 'react'
-import{View, Text, TextInput, Image, StyleSheet, useWindowDimensions, ScrollView} from 'react-native'
-import myCity from '../../assets/myCity.jpeg'
-import CustomButton from '../CustomButton'
-import CustomInput from '../CustomInput'
-import SocialSignInButtons from '../SocialSignInButtons/SocialSignInButtons'
-import { useNavigation } from '@react-navigation/core'
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
-import { loadUserToState } from '../../redux/user'
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
+import myCity from "../../assets/myCity.jpeg";
+import CustomButton from "../CustomButton";
+import CustomInput from "../CustomInput";
+import SocialSignInButtons from "../SocialSignInButtons/SocialSignInButtons";
+import { useNavigation } from "@react-navigation/core";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { loadUserToState } from "../../redux/user";
 
 const SignInScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const{height} = useWindowDimensions();
+  const { height } = useWindowDimensions();
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
+  const onSignInPressed = async () => {
+    const user = await axios.post(
+      `https://my-city-server.herokuapp.com/auth/login`,
+      { email, password }
+    );
+    console.log(user);
+    dispatch(loadUserToState(email));
 
-  const onSignInPressed =  async () =>{
+    console.log("user", user.data);
 
-
-      const user = await axios.post(`https://my-city-server.herokuapp.com/auth/login`, {email, password})
-      console.log(user.data)
-      dispatch(loadUserToState(email))
-
-    if(user.data['token']){
-    navigation.navigate('Home')
-    }else{
-      console.warn('incorrect password')
+    if (user.data["token"]) {
+      navigation.navigate("Home");
+    } else {
+      console.warn("incorrect password");
     }
-  }
+  };
 
-  const onForgotPassWordPressed = () =>{
+  const onForgotPassWordPressed = () => {
+    navigation.navigate("Forgot Password");
+  };
 
-    navigation.navigate('Forgot Password')
-  }
-
-
-
-  const onSignUpPressed = () =>{
-
-    navigation.navigate('signUp')
-  }
-
-
+  const onSignUpPressed = () => {
+    navigation.navigate("signUp");
+  };
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={styles.root}>
-      <Image source={myCity}
-       style={[styles.myCity, {height: height * 0.3}]}
-       resizeMode="contain" />
+      <View style={styles.root}>
+        <Image
+          source={myCity}
+          style={[styles.myCity, { height: height * 0.3 }]}
+          resizeMode='contain'
+        />
 
-       <TextInput
-        style={styles.container}
-        placeholder='username'
-        name="username"
-        autoCapitalize="none"
-        value={email}
-        onChangeText={email => setEmail(email)}
-      />
-      <TextInput
-        style={styles.container}
-        placeholder='password'
-        name="password"
-        autoCapitalize="none"
-        password
-        value={password}
-        onChangeText={password => setPassword(password)}
-      />
+        <TextInput
+          style={styles.container}
+          placeholder='username'
+          name='username'
+          autoCapitalize='none'
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+        <TextInput
+          style={styles.container}
+          placeholder='password'
+          name='password'
+          autoCapitalize='none'
+          password
+          value={password}
+          onChangeText={(password) => setPassword(password)}
+        />
 
-      {/* <CustomInput placeholder="UserName"  value={username}
+        {/* <CustomInput placeholder="UserName"  value={username}
         onChangeText={username => setUsername(username)}/>
       <CustomInput placeholder="Password" value={password}
         onChangeText={password => setPassword(password)}
        secureTextEntry
        /> */}
-      <CustomButton text='Sign In' type='signIn' onPress={onSignInPressed}/>
-      <CustomButton text='Forgot Password?' onPress={onForgotPassWordPressed} type="forgot"/>
-      <SocialSignInButtons/>
-      <CustomButton text="Sign Up" onPress={onSignUpPressed} type="forgot"/>
-    </View>
+        <CustomButton text='Sign In' type='signIn' onPress={onSignInPressed} />
+        <CustomButton
+          text='Forgot Password?'
+          onPress={onForgotPassWordPressed}
+          type='forgot'
+        />
+        <SocialSignInButtons />
+        <CustomButton text='Sign Up' onPress={onSignUpPressed} type='forgot' />
+      </View>
     </ScrollView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  root:{
-    alignItems:'center',
+  root: {
+    alignItems: "center",
     padding: 20,
   },
-  myCity:{
-    width:'70%',
+  myCity: {
+    width: "70%",
     maxWidth: 300,
-    maxHeight: 300
+    maxHeight: 300,
   },
-  container:{
-    backgroundColor: 'white',
-    width: '100%',
+  container: {
+    backgroundColor: "white",
+    width: "100%",
 
-    borderColor: '#e8e8e8',
+    borderColor: "#e8e8e8",
     borderWidth: 1,
     borderRadius: 5,
 
     paddingHorizontal: 5,
-    marginVertical: 2
+    marginVertical: 2,
   },
-})
+});
 
-
-export default SignInScreen
+export default SignInScreen;
