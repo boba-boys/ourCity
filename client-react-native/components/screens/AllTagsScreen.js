d87552e64ca903c55e8b0b47d4988a4e679e1149
bradleyplunkett <46577623+bradleyplunkett@users.noreply.github.com>
2021-12-08 12:32:34 -0500

    Revert "Revert "Got the Pin color to change, and some styling updates""

--- client-react-native/components/screens/AllTagsScreen.js
+++ client-react-native/components/screens/AllTagsScreen.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import Carousel from "react-native-snap-carousel";
 import { getAllTagsScreenStatus } from "../../redux/allTagsScreenStatus";
+import { HoverTagFunc } from "../../redux/tagHover";
 
 const SLIDER_WIDTH = Dimensions.get("window").width;
 const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
@@ -25,6 +26,9 @@ const AllTagsScreen = (props) => {
   // Redux store
   const userTags = useSelector((state) => state.tags);
   const tagsStatus = useSelector((state) => state.allTagsScreenStatus);
+  const globalState = useSelector((state) => state);
+
+
 
   const Separator = () => <View style={styles.separator} />;
 
@@ -38,7 +42,7 @@ const AllTagsScreen = (props) => {
         <Separator />
         <View>
           <Image
-            source={{ uri: "https://i.imgur.com/7k7nFm7.png" }}
+            source={{ uri: "https://assets3.thrillist.com/v1/image/2679747/2880x1620/crop;webp=auto;jpeg_quality=60;progressive.jpg" }}
             style={styles.image}
           />
           {/*This images should come from the Google API places */}
@@ -73,14 +77,21 @@ const AllTagsScreen = (props) => {
   };
 
   const onCarouselItemChange = (index) => {
-    let location = userTags[index];
+    let pin = userTags[index];
+    // console.log('below is the pin you are on right now', pin)
+
+    //create some redux stuff, add this pin to the global state.
     props.mapRef.current.animateToRegion({
-      latitude: location.latitude,
-      longitude: location.longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+      latitude: pin.latitude,
+      longitude: pin.longitude,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.05,
     });
+    // console.log('this is what pin looks like', pin)
+dispatch(HoverTagFunc(pin.id));
+console.log('this is what tags looks like', globalState.tags)
   };
+
 
   return (
     <View>
@@ -139,7 +150,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   tagName: {
-    color: "#222",
+    color: "black",
     fontSize: 28,
     fontWeight: "bold",
     alignSelf: "center",
--- client-react-native/components/screens/CreateGroup.js
+++ client-react-native/components/screens/CreateGroup.js
@@ -30,7 +30,7 @@ const CreateGroup = () => {
   const [groupName, setGroupName] = useState("");
   const [groupDescription, setGroupDescription] = useState("");
   const [groupImage, setGroupImage] = useState(
-    "https://i.imgur.com/7k7nFm7.png"
+    "https://rockymountevents.com/wp-content/uploads/2018/09/birthday_celebration.jpg"
   );
   const navigation = useNavigation();
 
