21ebc5142d86c338313a173cdc846843aeea57cb
hector nevarez <hanevarezg@gmail.com>
2021-12-07 21:07:25 -0800

    Not able to proper pass search oitem to global state

--- client-react-native/redux/searchResultsOnState.js
+++ client-react-native/redux/searchResultsOnState.js
@@ -1,4 +1,5 @@
 const SET_SEARCH_ON_STATE = "SET_SEARCH_ON_STATE";
+const PRESSED_SEARCH_ON_STATE= 'PRESSED_SEARCH_ON_STATE';
 
 // action creators
 const setSearchResult = (results) => {
@@ -8,12 +9,27 @@ const setSearchResult = (results) => {
   };
 };
 
+const getSearchResult = (results) => {
+  return {
+    type: PRESSED_SEARCH_ON_STATE,
+    pressedResult: results,
+  };
+};
+
 // thunks
 export const setSearchOnState = (results) => async (dispatch) => {
   try {
-
-
     dispatch(setSearchResult(results))
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+};
+
+// This sets the pressed result in the global state
+export const getSearchOnState = (resultPressed) => async (dispatch) => {
+  try {
+    dispatch(getSearchResult(resultPressed))
   } catch (err) {
     console.log(err);
     return [];
@@ -25,6 +41,8 @@ export default function setSearchResultsOnState(state = [], action) {
   switch (action.type) {
     case SET_SEARCH_ON_STATE:
       return action.currentResults;
+    case PRESSED_SEARCH_ON_STATE:
+      return action.pressedResult;
     default:
       return state;
   }
